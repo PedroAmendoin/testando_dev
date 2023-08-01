5eaec43e78a0b7f1cf90cc96d0224b82931c2ad6
PedroAmendoin <135077445+PedroAmendoin@users.noreply.github.com>
2023-07-31 22:12:12 -0300

    usuario perfil

---
+++ PrimeirotesteDS-main/Modelo/UsuarioModelo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    //objeto usuário
+    public class UsuarioModelo
+    {
+        //atritubos aplicados no bd
+        public int idusuario;
+        public string nome;
+        public string senha;
+        public int idperfil;
+        //construtor da classe modelo usuario
+        public UsuarioModelo()
+        {
+            nome = null;
+            senha = null;
+            idperfil = 0;
+        }
+    }
+}
